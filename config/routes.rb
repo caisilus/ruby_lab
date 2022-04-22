e7b6771fefdc33ed707903fb36abfe2fb259015d
caisilus <serangor@gmail.com>
2022-04-22 18:20:06 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,30 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'course#index'
+
+  # authentication
+  # get '/auth', to: 'auth#new'
+  # post '/auth', to: "auth#create"
+  # post '/auth/microsoft', to: 'auth#microsoft'
+  # post '/auth/github', to: 'auth#github'
+  # delete '/auth', to: 'auth#destroy'
+
+  # registration
+  # post '/users', to: 'users#create'
+
+  get '/course', to: 'course#index' # general course information
+
+  # labs
+  get '/labs/:id', to: 'labs#show', as: 'lab'
+  get '/labs', to: 'labs#index'
+
+  # tasks
+  get '/tasks/new', to: 'task#new'
+  post '/tasks', to: 'task#create'
+
+  # ------------ for logged in users ------------
+
+  # results
+  get '/results', to: 'results#index'
 end
