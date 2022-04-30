c56606ef0a1f7917e1016bb6c00a4a04cfea2204
caisilus <serangor@gmail.com>
2022-04-30 16:29:51 +0300

    First version of tailwindui navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,16 @@
 module ApplicationHelper
-  def nav_link(name, url)
-    link_to name, url, data: { action: "navbar#enable_section",
-                               "navbar-target": "section" }
+  def nav_link(name, url, selected)
+    link_to name, url,
+            class: "#{name == selected ? current_nav_class : default_nav_class} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+  end
+
+  private
+
+  def current_nav_class
+    "border-indigo-500 text-gray-900"
+  end
+
+  def default_nav_class
+    "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
   end
 end
