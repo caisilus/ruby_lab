ff4212c4d47adb5af89fed623bbbebd19e0026fb
caisilus <serangor@gmail.com>
2022-05-02 23:56:41 +0300

    Add sidebar mobile folding animations

--- app/javascript/controllers/sidebar_animation_controller.js
+++ app/javascript/controllers/sidebar_animation_controller.js
@@ -1,19 +1,70 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["sidebarMenu", "showingStart", "showingEnd", "hidingStart", "hidingEnd"]
+    static targets = ["sidebarMenu", "canvasOverlay", "canvasMenu", "closeButton"]
+    static values = {
+        canvasStart: String,
+        canvasEnd: String,
+        canvasMenuStart: String,
+        canvasMenuEnd: String,
+        closeButtonStart: String,
+        closeButtonEnd: String,
+        duration: Number
+    }
 
     initialize() {
         this.isShown = false
     }
 
-    startAnimation() {
+    async startAnimation() {
         console.log("toggle")
 
-        if (this.isShown)
+        if (this.isShown) {
+            this.hideTransition()
+
+            await this.timeout(this.durationValue)
+
             this.sidebarMenuTarget.classList.add("hidden")
-        else
+        }
+        else {
             this.sidebarMenuTarget.classList.remove("hidden")
+
+            await this.nextFrame()
+
+            this.showTransition()
+        }
         this.isShown = !this.isShown
+    }
+
+    hideTransition() {
+        this.canvasOverlayTarget.classList.remove(this.canvasEndValue)
+        this.canvasOverlayTarget.classList.add(this.canvasStartValue)
+        this.canvasMenuTarget.classList.remove(this.canvasMenuEndValue)
+        this.canvasMenuTarget.classList.add(this.canvasMenuStartValue)
+        this.closeButtonTarget.classList.remove(this.closeButtonEndValue)
+        this.closeButtonTarget.classList.add(this.closeButtonStartValue)
+    }
+
+    showTransition() {
+        this.canvasOverlayTarget.classList.remove(this.canvasStartValue)
+        this.canvasOverlayTarget.classList.add(this.canvasEndValue)
+        this.canvasMenuTarget.classList.remove(this.canvasMenuStartValue)
+        this.canvasMenuTarget.classList.add(this.canvasMenuEndValue)
+        this.closeButtonTarget.classList.remove(this.closeButtonStartValue)
+        this.closeButtonTarget.classList.add(this.closeButtonEndValue)
+    }
+
+    nextFrame() {
+        return new Promise(resolve => {
+            requestAnimationFrame(() => {
+                requestAnimationFrame(resolve)
+            })
+        })
+    }
+
+    timeout(ms) {
+        return new Promise(resolve =>{
+            setTimeout(resolve, ms)
+        })
     }
 }
