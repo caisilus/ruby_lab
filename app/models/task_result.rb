29465f70a8a00ed2b073b729588f8afcd9b5682e
caisilus <serangor@gmail.com>
2022-05-03 18:56:12 +0300

    Switch to asyncronious version of model broadcast

--- app/models/task_result.rb
+++ app/models/task_result.rb
@@ -2,7 +2,7 @@ class TaskResult < ApplicationRecord
   belongs_to :task
   validates  :passed_tests, :total_tests, presence: true
 
-  after_update_commit { broadcast_update }
+  after_update_commit { broadcast_update_later }
 
   def passed_percentage
     passed_tests * 100.0 / total_tests
