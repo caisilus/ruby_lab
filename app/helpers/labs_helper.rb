5cfd98443198aaf798c7a90a4485bd5b019ec419
caisilus <serangor@gmail.com>
2022-05-03 13:58:28 +0300

    Add realtime updates for result model

--- app/helpers/labs_helper.rb
+++ app/helpers/labs_helper.rb
@@ -2,8 +2,6 @@ module LabsHelper
   def task_template(template, task)
     percentage = task.last_result&.passed_percentage&.round(2)
     percentage = 0 if percentage.nil?
-    render partial: template, locals: { title: task.title,
-                                        text: task.text,
-                                        percentage: percentage }
+    render partial: template, locals: { task: task }
   end
 end
--- app/models/task_result.rb
+++ app/models/task_result.rb
@@ -2,6 +2,8 @@ class TaskResult < ApplicationRecord
   belongs_to :task
   validates  :passed_tests, :total_tests, presence: true
 
+  after_update_commit { broadcast_update }
+
   def passed_percentage
     passed_tests * 100.0 / total_tests
   end
