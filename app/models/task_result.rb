0bb5da29cec71d989bcfc999a6d70b068289ce46
caisilus <serangor@gmail.com>
2023-04-15 20:04:04 +0300

    Change turbo stream for task result from update to create

--- app/models/task_result.rb
+++ app/models/task_result.rb
@@ -2,10 +2,15 @@ class TaskResult < ApplicationRecord
   belongs_to :task
   validates  :passed_tests, :total_tests, presence: true
 
-  after_update_commit { broadcast_update_later }
+  # after_update_commit { broadcast_update_later }
+  after_create_commit :broadcast_later
 
   def passed_percentage
     passed_tests * 100.0 / total_tests
+  end
+
+  def broadcast_later
+    broadcast_replace_later_to(task, :task_results, target: "last_result_task_#{task_id}")
   end
 end
 
