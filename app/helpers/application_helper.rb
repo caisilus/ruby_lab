d4fb8da68fce5534c4e43946b266e52a6fe9392c
caisilus <serangor@gmail.com>
2023-04-16 19:34:28 +0300

    Minor refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   def nav_link(name, url, selected)
     link_to name, url,
-            class: "#{name == selected ? current_nav_class : default_nav_class} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+            class: "#{name == selected ? current_nav_class : default_nav_class} #{base_nav_class}"
   end
 
   def mobile_nav_link(name, url, selected)
     link_to name, url,
-            class: "#{name == selected ? current_nav_mobile_class : default_nav_mobile_class} block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+            class: "#{name == selected ? current_nav_mobile_class : default_nav_mobile_class} #{base_nav_mobile_class}"
   end
 
   private
@@ -19,11 +19,19 @@ module ApplicationHelper
     "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
   end
 
+  def base_nav_class
+    "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+  end
+
   def current_nav_mobile_class
     "bg-indigo-50 border-indigo-500 text-indigo-700"
   end
 
   def default_nav_mobile_class
     "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
+  end
+
+  def base_nav_mobile_class
+    "block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
   end
 end
--- app/helpers/labs_helper.rb
+++ app/helpers/labs_helper.rb
@@ -1,7 +1,2 @@
 module LabsHelper
-  def task_template(template, task)
-    percentage = task.last_result&.passed_percentage&.round(2)
-    percentage = 0 if percentage.nil?
-    render partial: template, locals: { task: task }
-  end
 end
