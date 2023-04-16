ed3ff4e5832e9170c2816214c1598a20ffb3850d
caisilus <serangor@gmail.com>
2023-04-16 19:13:58 +0300

    Fix bug with disposable turbo streams

--- app/models/task_result.rb
+++ app/models/task_result.rb
@@ -2,7 +2,6 @@ class TaskResult < ApplicationRecord
   belongs_to :task
   validates  :passed_tests, :total_tests, presence: true
 
-  # after_update_commit { broadcast_update_later }
   after_create_commit :broadcast_later
 
   def passed_percentage
@@ -10,7 +9,7 @@ class TaskResult < ApplicationRecord
   end
 
   def broadcast_later
-    broadcast_replace_later_to(task, :task_results, target: "last_result_task_#{task_id}")
+    broadcast_update_later_to(task, :task_results, target: "last_result_task_#{task_id}")
   end
 end
 
