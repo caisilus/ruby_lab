76fd12f064cc6b47162d2aac3bbbaad7ea944103
caisilus <serangor@gmail.com>
2023-04-22 15:40:24 +0300

    Add avatar_url to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :github_login, presence: true, uniqueness: true
   validates :name, presence: true
   validates :surname, presence: true
+  validates :avatar_url, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_123926) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_22_112452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_123926) do
     t.string "repo_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_url", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
