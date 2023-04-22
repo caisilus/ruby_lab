4ba7d7134f92846794e72692bfc5857db867da07
caisilus <serangor@gmail.com>
2023-04-22 19:44:25 +0300

    Add log in button to navbar when current user is nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,15 @@ class ApplicationController < ActionController::Base
   def set_current_user
     if session[:current_user_id].nil?
       @current_user = nil
+      setup_github_auth_link
       return
     end
 
     @current_user = User.find(session[:current_user_id])
+  end
+
+  def setup_github_auth_link
+    @authorize_url = Octokit::Client.new.authorize_url ENV["GITHUB_CLIENT_ID"], options = { scope: "repo" }
   end
 
   def not_found
