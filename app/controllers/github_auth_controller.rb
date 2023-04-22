60d8d54d305778215129c9362eae9af670bfda4a
caisilus <serangor@gmail.com>
2023-04-22 15:42:51 +0300

    Add redirect to registration after github log in and change navbar to show logged user's avatar

--- app/controllers/github_auth_controller.rb
+++ app/controllers/github_auth_controller.rb
@@ -9,7 +9,16 @@ class GithubAuthController < ApplicationController
     code = params[:code]
     result = Octokit::Client.new.exchange_code_for_token(code.to_s, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'],
                                                          {accept: 'application/json'})
+
+    raise ActionController::InvalidAuthenticityToken unless result.key?(:access_token)
+
+    client = Octokit::Client.new(access_token: result[:access_token])
+    user = User.find_by(github_login: client.login)
+
     session[:github_access_token] = result[:access_token]
-    redirect_to repository_new_url
+    return redirect_to users_new_url if user.nil?
+
+    session[:current_user] = user
+    redirect_to labs_url
   end
 end
