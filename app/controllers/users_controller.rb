6e00f60336fe8a887f7fa204647d5454f437880b
caisilus <serangor@gmail.com>
2023-06-03 00:26:25 +0300

    Try to add error handling for users/new form. Unsuccessfully :(

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,21 @@
 class UsersController < ApplicationController
   def new
+    render :new, locals: { error_messages: ["AAAAAAAAAA"] }
   end
 
   def create
-    github_user = Octokit::Client.new(access_token: session[:github_access_token]).user
-
-    parsed_params = parse_params
-
+    parsed_params = parse_params.merge(github_user)
     user = User.new parsed_params
-    user.github_login = github_user[:login]
-    user.avatar_url = github_user[:avatar_url]
 
     link = parsed_params[:repo_link]
 
-    subscribe_to_repo link
+    success = subscribe_to_repo(link)
+
+    return render :new, locals: { error_messages: ["Incorrect link to repo!"] } unless success
 
     user.repo_link = link.gsub("https://github.com/", "")
-    user.save
+
+    return render :new, locals: { error_messages: user.errors.full_messages }  unless user.save
 
     session[:current_user_id] = user.id
 
@@ -29,8 +28,18 @@ class UsersController < ApplicationController
     params.permit(:surname, :name, :middle_name, :repo_link)
   end
 
+  def github_user
+    github_user = Octokit::Client.new(access_token: session[:github_access_token]).user
+
+    { github_login: github_user[:login], avatar_url: github_user[:avatar_url] }
+  end
+
   def subscribe_to_repo(link)
-    client = Octokit::Client.new access_token: session[:github_access_token]
-    client.subscribe("#{link}/events/push", Current.host_url + payload_path)
+    client = Octokit::Client.new(access_token: session[:github_access_token])
+    begin
+      client.subscribe("#{link}/events/push", Current.host_url + payload_path)
+    rescue Octokit::UnprocessableEntity => e
+      false
+    end
   end
 end
