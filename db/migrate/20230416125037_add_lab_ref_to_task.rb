3631328899f0bd516f353343b65675f757c9d80d
caisilus <serangor@gmail.com>
2023-06-08 10:33:52 +0300

    Fix migrations to run on empty db

--- db/migrate/20230416125037_add_lab_ref_to_task.rb
+++ db/migrate/20230416125037_add_lab_ref_to_task.rb
@@ -1,8 +1,12 @@
 class AddLabRefToTask < ActiveRecord::Migration[7.0]
   def up
-    add_reference :tasks, :lab, foreign_key: true
-    Task.update_all(lab_id: Lab.first.id)
-    change_column_null :tasks, :lab_id, false
+    if Lab.any?
+      add_reference :tasks, :lab, foreign_key: true
+      Task.update_all(lab_id: Lab.first.id)
+      change_column_null :tasks, :lab_id, false
+    else
+      add_reference :tasks, :lab, foreign_key: true, null: false
+    end
   end
 
   def down
--- db/migrate/20230424104840_add_user_ref_to_task_result.rb
+++ db/migrate/20230424104840_add_user_ref_to_task_result.rb
@@ -1,8 +1,12 @@
 class AddUserRefToTaskResult < ActiveRecord::Migration[7.0]
   def up
-    add_reference :task_results, :user, foreign_key: true
-    TaskResult.update_all(user_id: User.first.id)
-    change_column_null :task_results, :user_id, false
+    if User.any?
+      add_reference :task_results, :user, foreign_key: true
+      TaskResult.update_all(user_id: User.first.id)
+      change_column_null :task_results, :user_id, false
+    else
+      add_reference :task_results, :user, foreign_key: true, null: false
+    end
   end
 
   def down
